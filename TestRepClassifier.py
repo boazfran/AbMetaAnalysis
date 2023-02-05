502ff43fd365b539f9d067a9c85aceb5065cbf2d
boazfran <boazfr@gmail.com>
2023-02-05 11:33:51 +0200

    variable renaming - results->result_metrics and subject_table->result_folds. change functions api to have one list of feature_selection_cfg_values instead of several list arguments each with different cfg parameter.

--- TestRepClassifier.py
+++ TestRepClassifier.py
@@ -107,23 +107,21 @@ def test_fold(
         ]
         print(result_metrics.iloc[-1])
 
-            # collect statistics on selected clusters and subjects classification
-            subject_table.loc[
-                np.array(test_labels.index.tolist() + ['case_th', 'ctrl_th', 'dist_th', 'fs_mode', 'k', 'kmer_clustering'], dtype=object), i
-            ] = (test_labels == predict_labels).to_list() + [case_th, ctrl_th, dist_th, fs_mode, k, kmer2cluster is not None]
+        # collect statistics on selected clusters and subjects classification
+        result_folds.loc[
+            np.array(test_labels.index.tolist() + ['case_th', 'ctrl_th', 'dist_th', 'fs_method', 'k', 'kmer_clustering'], dtype=object), i
+        ] = (test_labels == predict_labels).to_list() + [case_th, ctrl_th, dist_th, fs_method, k, kmer2cluster is not None]
 
-    return results, subject_table.transpose()
+    return result_metrics, result_folds.transpose()
 
 
 @ray.remote(max_retries=0)
 def remote_test_fold(
-        airr_seq_df, train_labels, test_labels, dist_mat_dir, dist_th, case_th, ctrl_th,
-        feature_selection_mode_values, k_values, kmer2cluster_values
+    airr_seq_df, train_labels, test_labels, dist_mat_dir, dist_th, case_th, ctrl_th, feature_selection_cfg_values
 ):
 
     return test_fold(
-        airr_seq_df, train_labels, test_labels, dist_mat_dir, dist_th, case_th, ctrl_th,
-        feature_selection_mode_values, k_values, kmer2cluster_values,
+        airr_seq_df, train_labels, test_labels, dist_mat_dir, dist_th, case_th, ctrl_th, feature_selection_cfg_values
     )
 
 
@@ -137,9 +135,7 @@ def test_rep_classifier(
     case_th_values: list = [1],
     ctrl_th_values: list = [0],
     dist_th_values: list = [0.2],
-    feature_selection_mode_values: list = ['naive'],
-    k_values: list = [],
-    kmer2cluster_values: list = []
+    feature_selection_cfg_values: list = [{'method': 'naive'}],
 ):
     """
 
@@ -152,14 +148,13 @@ def test_rep_classifier(
     :param case_th_values: list of shared case repertoire samples threshold for the feature selection
     :param ctrl_th_values: list of shared ctrl repertoire samples threshold for the feature selection
     :param dist_th_values: list of normalized hamming distance cut off values for the hierarchical clustering
-    :param feature_selection_mode_values: list of method to use for the feature selection ("naive" or "similar")
-    :param k_values: list of k values to use for the k-mers segmentation. relevant only when feature selection mode is similar
-    :param kmer2cluster_values: list of k-mers to k-mers cluster id maps relevant only when feature selection mode is similar
+    :param feature_selection_cfg_values: list of dictionaries with feature selection configuration
     :return: (data frame with the folds classification results, data frame with the folds test samples)
     """
     airr_seq_df = ray.put(airr_seq_df)
-    for i in range(len(kmer2cluster_values)):
-        kmer2cluster_values[i] = ray.put(kmer2cluster_values[i])
+    for fs_cfg in feature_selection_cfg_values:
+        if 'kmer_cluster_map' in fs_cfg:
+            fs_cfg['kmer2cluster'] = ray.put(fs_cfg['kmer2cluster'])
     result_ids = []
     rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=42)
     for train_index, validation_index in rskf.split(labels, labels):
@@ -175,166 +170,60 @@ def test_rep_classifier(
                 for dist_th in dist_th_values:
                     result_ids.append(
                         remote_test_fold.remote(
-                            airr_seq_df, train_labels, validation_labels, dist_mat_dir, dist_th, case_th, ctrl_th,
-                            feature_selection_mode_values, k_values, kmer2cluster_values
+                            airr_seq_df, train_labels, validation_labels, dist_mat_dir, dist_th, case_th, ctrl_th, feature_selection_cfg_values
                         )
                     )
 
-    results, subject_table = pd.DataFrame(), pd.DataFrame()
+    result_metrics, result_folds = pd.DataFrame(), pd.DataFrame()
     for result_id in result_ids:
-        results_itr, subject_table_itr = ray.get(result_id)
-        results = results.append(results_itr, ignore_index=True)
-        subject_table = subject_table.append(subject_table_itr, ignore_index=True)
+        result_metrics__itr, result_folds_itr = ray.get(result_id)
+        result_metrics = result_metrics.append(result_metrics__itr, ignore_index=True)
+        result_folds = result_folds.append(result_folds_itr, ignore_index=True)
 
-    return results, subject_table
+    return result_metrics, result_folds
 
 
 def save_results(
-    results: pd.DataFrame, subject_table:pd.DataFrame, output_dir: str, base_name: str
+    result_metrics: pd.DataFrame, result_folds: pd.DataFrame, output_dir: str, base_name: str
 ):
     """
     group the classification result data frames by hyper-parameters and saves frames as different files
-    :param results: the data frame with the classification metrics
-    :param subject_table: the data frame with the folds samples split information
+    :param result_metrics: the data frame with the classification metrics
+    :param result_folds: the data frame with the folds samples split information
     :param output_dir: output directory to save the files
     :param base_name: prefix for the saved files
     :return:
     """
-    hyper_parameters = results.head(0).drop(columns=['support', 'accuracy_score', 'recall_score', 'precision_score', 'f1-score']).columns.tolist()
+    hyper_parameters = result_metrics.head(0).drop(columns=['support', 'accuracy_score', 'recall_score', 'precision_score', 'f1-score']).columns.tolist()
 
-    for params, frame in results.groupby(hyper_parameters):
+    for params, frame in result_metrics.groupby(hyper_parameters):
         output_path = os.path.join(
             output_dir, '_'.join([base_name] + [f'{key}-{val}' for key, val in zip(hyper_parameters, params)]) + '_results.csv'
         )
         frame.drop(columns=hyper_parameters).to_csv(output_path, index=False)
 
-    for params, frame in subject_table.groupby(hyper_parameters):
+    for params, frame in result_folds.groupby(hyper_parameters):
         output_path = os.path.join(
             output_dir, '_'.join([base_name] + [f'{key}-{val}' for key, val in zip(hyper_parameters, params)]) + '_subject_table.csv'
         )
         frame.drop(columns=hyper_parameters).to_csv(output_path, index=False)
 
 
-def test_by_n_clusters(
-    input_dir: str,
-    output_dir: str,
-    case_metadata: pd.DataFrame,
-    ctrl_metadata: pd.DataFrame,
-    labels: pd.Series,
-    train_only_labels: pd.Series = None,
-    n_splits: int = 10,
-    n_repeats: int = 10,
-    v_call_field: str = "v_call_original",
-    group_mode: str = "family",
-    max_seq_per_cluster_values: list = [1],
-    n_clusters_values: list = [100],
-    case_th_values: list = [1],
-    ctrl_th_values: list = [0],
-    dist_th_values: list = [0.2],
-    feature_selection_mode_values: list = ['naive'],
-    within_sample_clustering_values: list = ['complete_linkage_0.0'],
-    k_values: list = [],
-    kmer2cluster_values: list = []
+def load_results(
+    results_dir: str,
+    base_name: str,
+    hyper_parameters_dict
 ):
-    """
-    :param input_dir:
-    :param output_dir:
-    :param case_metadata:
-    :param ctrl_metadata:
-    :param labels:
-    :param train_only_labels: labels of repertoire samples that can only be used for training set and not for test
-    :param n_splits:
-    :param n_repeats:
-    :param v_call_field:
-    :param group_mode:
-    :param max_seq_per_cluster_values:
-    :param n_clusters_values:
-    :param case_th_values:
-    :param ctrl_th_values:
-    :param dist_th_values:
-    :param feature_selection_mode_values:
-    :param within_sample_clustering_values:
-    :param k_values:
-    :param kmer2cluster_values:
-    :return:
-    """
-    for within_sample_clustering in within_sample_clustering_values:
-        for n_clusters in n_clusters_values:
-            for max_seq_per_cluster in max_seq_per_cluster_values:
-                base_name = f'top_{n_clusters}_{within_sample_clustering}_clusters_max_seq_per_cluster_{max_seq_per_cluster}'
-                airr_seq_df_file_path = os.path.join(output_dir, 'sampled_datasets', base_name + '.tsv')
-                if not os.path.isfile(airr_seq_df_file_path):
-                    sample_by_n_clusters(
-                        case_metadata.append(ctrl_metadata),
-                        input_dir,
-                        os.path.join(output_dir, 'sampled_datasets'),
-                        [n_clusters],
-                        [max_seq_per_cluster],
-                        [f'subject_cluster_id_{within_sample_clustering}']
-                    )
-                airr_seq_df = load_sampled_airr_seq_df(airr_seq_df_file_path, v_call_field=v_call_field, group_mode=group_mode)
-                save_distance_matrices(airr_seq_df, os.path.join(output_dir, "distance_matrices", base_name))
-                results, subject_table = test_rep_classifier(
-                    airr_seq_df,
-                    labels,
-                    os.path.join(output_dir, "distance_matrices", base_name),
-                    train_only_labels,
-                    n_splits,
-                    n_repeats,
-                    case_th_values,
-                    ctrl_th_values,
-                    dist_th_values,
-                    feature_selection_mode_values,
-                    k_values,
-                    kmer2cluster_values
-                )
-                save_results(results, subject_table, os.path.join(output_dir, 'rep_clf_output'), base_name)
+    result_metrics, result_folds = None, None
+    results_file_path = os.path.join(
+        results_dir, '_'.join([base_name] + [f'{key}-{val}' for key, val in hyper_parameters_dict.items()]) + '_results.csv'
+    )
+    if os.path.isfile(results_file_path):
+        result_metrics = pd.read_csv(results_file_path)
+    folds_file_path = os.path.join(
+        results_dir, '_'.join([base_name] + [f'{key}-{val}' for key, val in hyper_parameters_dict.items()]) + '_folds.csv'
+    )
+    if os.path.isfile(folds_file_path):
+        result_folds = pd.read_csv(folds_file_path)
 
-
-def test_by_n_sequences(
-        case_metadata,
-        ctrl_metadata,
-        labels,
-        input_dir,
-        output_dir,
-        n_repeats: int = 10,
-        n_splits: int = 10,
-        v_call_field: str = "v_call_original",
-        group_mode: str = "family",
-        n_sequences_values: list = [100],
-        case_th_values: list = [1],
-        ctrl_th_values: list = [0],
-        dist_th_values: list = [0.2],
-        feature_selection_mode_values: list = ['naive'],
-        k_values: list = [],
-        kmer2cluster_values: list = []
-):
-
-    for n_sequences in n_sequences_values:
-        base_name = f'random_{n_sequences}_sequences_samples'
-        airr_seq_df_file_path = os.path.join(output_dir, 'sampled_datasets', base_name + '.tsv')
-        if not os.path.isfile(airr_seq_df_file_path):
-            sample_by_n_sequences(
-                case_metadata.append(ctrl_metadata),
-                input_dir,
-                output_dir,
-                [n_sequences]
-            )
-        save_distance_matrices(airr_seq_df, os.path.join(output_dir, "distance_matrices", base_name))
-        airr_seq_df = load_sampled_airr_seq_df(airr_seq_df_file_path, v_call_field=v_call_field, group_mode=group_mode)
-        results, subject_table = test_rep_classifier(
-            airr_seq_df,
-            labels,
-            os.path.join(output_dir, "distance_matrices", base_name),
-            n_splits,
-            n_repeats,
-            case_th_values,
-            ctrl_th_values,
-            dist_th_values,
-            feature_selection_mode_values,
-            k_values,
-            kmer2cluster_values
-        )
-        save_results(results, subject_table, os.path.join(output_dir, 'rep_clf_output'), base_name)
-
-
+    return result_metrics, result_folds
