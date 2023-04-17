00c7a8400529a85d9c7970057c4dca9c682fa8c0
boazfran <boazfr@gmail.com>
2023-04-17 23:46:45 +0300

    Fix bug when skipping subsampling because target file with the same name already exists.

--- AbMetaAnalysis/SubSample.py
+++ AbMetaAnalysis/SubSample.py
@@ -128,7 +128,7 @@ def subsample(
                 if not cfg["force"] and os.path.isfile(output_file_path):
                     print(f'file {output_file_path} already exists - skipping sampling')
                 continue
-            elif output_files[cfg_idx] is None:
+            elif pd.isna(output_files[cfg_idx]) is None:
                 continue
             if single_sample_airr_seq_df is None:
                 print(f'Sampling file {input_file_idx + 1}: {sample.input_file}')
